5b97ac063fb3d99158ba3fe2d8606a447fdfb231
Carlos-vini01 <carlosviniciusdonacimentosilva@gmail.com>
2025-09-12 22:29:51 -0300

    Completoo

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,31 @@
 describe('testar sistema de saude', () => {
-  it('Entrar no link cadastro', () => {
-    cy.visit('http://localhost:300/')
-    cy.get('[href="/cadastro"]') .click()
+ it('Entrar no link cadastro', () => {
+  cy.visit('http://localhost:3000/')
+  cy.get('[href="/cadastro"]') .click()
   })
-})
+});
+it("Preencher Nome", () => {
+  cy.visit("http://localhost:3000/");
+  cy.get('[href="/cadastro"]').click();
+  cy.get('[data-test="inputNome"]').type("Carlos Vinicius");
+});
+it("Preencher o CNPJ", () => {
+  cy.visit("http://localhost:3000/");
+  cy.get('[href="/cadastro"]').click();
+  cy.get('[data-test="inputCNPJ"]').type("548.762.565.5555-60");
+});
+it("Preencher o Email", () => {
+  cy.visit("http://localhost:3000/");
+  cy.get('[href="/cadastro"]').click();
+  cy.get('[data-test="inputEmail"]').type("carlosvini@gmail.com");
+});
+it("Preencher a Senha", () => {
+  cy.visit("http://localhost:3000/");
+  cy.get('[href="/cadastro"]').click();
+  cy.get('[data-test="inputSenha"]').type("12345678");
+});
+it("Preencher para Confimar a Senha", () => {
+  cy.visit("http://localhost:3000/");
+  cy.get('[href="/cadastro"]').click();
+  cy.get('[data-test="inputSenhaVerificada"]').type("12345678");
+});
